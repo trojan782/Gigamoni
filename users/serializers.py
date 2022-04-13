0c945bd82f24a404143731fb658c41eaf4506960
wonedav <wonderdavid2@gmail.com>
2022-04-13 09:51:14 +0100

    Edited the project

--- users/serializers.py
+++ users/serializers.py
@@ -1,17 +1,27 @@
+import phonenumbers
 from rest_framework import serializers
-from .models import GigaUser
+from .models import Person, PersonMore
 
-class RegisterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = GigaUser
-        fields = ('email', 'full_name', 'phone_number', 'password')
-        extra_kwargs = {'password': {'write_only': True}}
-        password = serializers.CharField(max_length=128, write_only=True, required=True)
+class PersonRegisterSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(read_only=True,) #by default allow_null = False
+    full_name = serializers.CharField(required=True)
+    phonenumbers = serializers.CharField(required=True)
 
-    def create(self, validated_data):
-        password = validated_data.pop('password', None)
-        instance = self.Meta.model(**validated_data)
-        if password is not None:
-            instance.set_password(password)
-        instance.save()
-        return instance
+    def get_cleaned_data(self):
+            data = super(PersonRegisterSerializer, self).get_cleaned_data()
+            extra_data = {
+                'full_name' : self.validated_data.get('full_name', ''),
+                'phone_number' : self.validated_data.get('phone_number', ''),
+            }
+            data.update(extra_data)
+            return data
+
+    def save(self, request):
+        user = super(PersonRegisterSerializer, self).save(request)
+        user.is_seller = True
+        user.save()
+        seller = Person(seller=user, area=self.cleaned_data.get('area'), 
+                address=self.cleaned_data.get('address'),
+                description=self.cleaned_data.get('description'))
+        seller.save()
+        return user
